c8e89911b8dd2da137248b8af040ad146b2c084d
Daniel9842 <69389536+Daniel9842@users.noreply.github.com>
2021-11-04 11:38:37 -0500

    Update SecurityConfiguration.java

--- src/main/java/edu/escuelaing/security/config/SecurityConfiguration.java
+++ src/main/java/edu/escuelaing/security/config/SecurityConfiguration.java
@@ -34,6 +34,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
                         .permitAll().antMatchers( HttpMethod.POST, "/user/stole" )
                         .permitAll().antMatchers( HttpMethod.GET, "/user/zone" )
                         .permitAll().antMatchers( HttpMethod.GET, "/user/verifyZone" )
+                        .permitAll().antMatchers( HttpMethod.GET, "/user/stolesInZone" )
                         .permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS );
 
