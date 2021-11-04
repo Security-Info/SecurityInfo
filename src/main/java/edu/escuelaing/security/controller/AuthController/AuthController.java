3617376e0959873f41a4f66680e6c812c6dc57ae
Duck James Alexander Torres Segura <69390068+JamesTorres99@users.noreply.github.com>
2021-11-04 04:14:13 -0500

    add cross
    ad

--- src/main/java/edu/escuelaing/security/controller/AuthController/AuthController.java
+++ src/main/java/edu/escuelaing/security/controller/AuthController/AuthController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Calendar;
 import java.util.Date;
@@ -22,6 +24,7 @@ import java.util.Date;
 import  static edu.escuelaing.security.utils.Constants.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping( "securityInfo/auth" )
 public class AuthController
 {
